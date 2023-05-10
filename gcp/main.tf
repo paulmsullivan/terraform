dbaf0ec2ff4b3d40d8fc547ac5eb75ab17545cde
paulmsullivan <paulmsullivan@outlook.com>
2023-05-10 15:24:49 -0400

    adding api enablement

--- gcp/main.tf
+++ gcp/main.tf
@@ -26,6 +26,10 @@ provider "google" {
   zone    = "us-central1-c"
 }
 
+resource "google_project_services" "project" {
+  services   = ["iam.googleapis.com", "cloudresourcemanager.googleapis.com"]
+}
+
 resource "google_project_iam_custom_role" "customVMStartStopv2" {
   role_id     = "customVMStartStopv2"
   title       = "Compute Instance Start and Stop"
