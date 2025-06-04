c2368cbf049bcb7146f9c10347ebeda3192217b5
paulmsullivan <paulmsullivan@outlook.com>
2025-06-04 15:44:16 -0500

    Update vpcscs.tf
    Signed-off-by: paulmsullivan <paulmsullivan@outlook.com>

--- gcp/vpcscs.tf
+++ gcp/vpcscs.tf
@@ -22,36 +22,51 @@ module "org_policy" {
   policy_name = "sample-vpc-sc-permimeter"
 }
 
-module "regular_service_perimeter_1" {
-  source                      = "terraform-google-modules/vpc-service-controls/google//modules/regular_service_perimeter"
-  policy                      = var.org_policy_name
-  perimeter_name              = "regular_perimeter_1"
-  description                 = "Perimeter shielding projects"
-  resources_dry_run           = ["127799619174"]
-  restricted_services_dry_run = ["bigquery.googleapis.com", "storage.googleapis.com"]
-  ingress_policies = [
-    {
-      title = "Allow Access from everywhere"
-      from = {
-        sources = {
-          access_levels = ["*"] # Allow Access from everywhere
-        },
-        identities = ["user:paulmsullivan@gmail.com"]
+resource "google_access_context_manager_service_perimeters" "service-perimeter" {
+  parent = "accessPolicies/"686487341936""
 
-      }
-      to = {
-        resources = [
-          "*"
-        ]
-        operations = {
-          "storage.googleapis.com" = {
-            methods = [
-              "google.storage.objects.get",
-              "google.storage.objects.list"
-            ]
-          }
-        }
-      }
+  service_perimeters {
+    name   = "accessPolicies/686487341936/servicePerimeters/"
+    title  = "title001"
+    status {
+      restricted_services = ["storage.googleapis.com"]
     }
-  ]
+  }
+
 }
+
+
+
+#  module "regular_service_perimeter_1" {
+#    source                      = "terraform-google-modules/vpc-service-controls/google# modules/regular_service_perimeter"
+#    policy                      = var.org_policy_name
+#    perimeter_name              = "regular_perimeter_1"
+#    description                 = "Perimeter shielding projects"
+#    resources_dry_run           = ["127799619174"]
+#    restricted_services_dry_run = ["bigquery.googleapis.com", "storage.googleapis.com"]
+#    ingress_policies = [
+#      {
+#        title = "Allow Access from everywhere"
+#        from = {
+#          sources = {
+#            access_levels = ["*"] # Allow Access from everywhere
+#          },
+#          identities = ["user:paulmsullivan@gmail.com"]
+
+#        }
+#        to = {
+#          resources = [
+#            "*"
+#          ]
+#          operations = {
+#            "storage.googleapis.com" = {
+#              methods = [
+#                "google.storage.objects.get",
+#                "google.storage.objects.list"
+#              ]
+#            }
+#          }
+#        }
+#      }
+#    ]
+#  }
