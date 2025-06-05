d375187384fe716a585164fcdb7ec5f3d208c348
paulmsullivan <paulmsullivan@outlook.com>
2025-06-05 09:18:08 -0500

    Update variables.tf
    Signed-off-by: paulmsullivan <paulmsullivan@outlook.com>

--- gcp/variables.tf
+++ gcp/variables.tf
@@ -7,3 +7,8 @@ variable "perimeter_name" {
   description = "perimeter name"
   type = string
 }
+
+variable "vpc_sc_services" {
+  description = "List of VPC SC restricted services"
+  type = list(string)
+}
