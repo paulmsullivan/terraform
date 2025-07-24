a2cd806a141a189f7e209a5e001a63f34dcf46b7
paulmsullivan <paulmsullivan@outlook.com>
2025-07-24 14:17:58 -0400

    Update main.tf
    Signed-off-by: paulmsullivan <paulmsullivan@outlook.com>

--- gcp/main.tf
+++ gcp/main.tf
@@ -149,17 +149,6 @@ resource "google_compute_disk_resource_policy_attachment" "attachment" {
   disk = google_compute_instance.paullab-vm1.name
 }
 
-resource "google_organization_policy" "public_ip_policy" {
-  org_id = "987000039256"
-  constraint = "compute.vmExternalIpAccess"
-
-  list_policy {
-    allow {
-      values = ["projects/cogent-dragon-379819/zones/us-central1-c/instances/paullab-vm1","projects/wordpress-446723/zones/us-central1-a/instances/wordpress-1-vm"]
-    }
-  }
-}
-
 #resource "google_org_policy_policy" "project_public_ip_policy" {
 #  name  = "projects/cogent-dragon-379819/policies/compute.vmExternalIPAccess"
 #  parent = "projects/cogent-dragon-379819"
