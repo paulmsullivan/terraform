8630f9c2d097dff0904264cddffdc2241b1e4a36
paulmsullivan <paulmsullivan@outlook.com>
2025-07-31 10:48:54 -0400

    Update iap.tf
    Signed-off-by: paulmsullivan <paulmsullivan@outlook.com>

--- gcp/iap.tf
+++ gcp/iap.tf
@@ -2,13 +2,13 @@
 # IF the user is connecting from an authorised network defined in the access 
 # list
 resource "google_iap_tunnel_iam_member" "allow-remote-access-to-iap" {
-    project = "<your-project-id>"
+    project = "cogent-dragon-379819"
     role    = "roles/iap.tunnelResourceAccessor"
-    member  = "user:calum.hunter@the.cloud"
+    member  = "user:paul.sullivan@sobekdigital.com"
 
     condition {
       title = "allow_remote_access_to_iap"
       description = "Allow access to IAP tunnel for authorized users"
-      expression = "\"accessPolicies/<access-policy-id>/accessLevels/<my-access-level-name>\" in request.auth.access_levels"
+#      expression = "\"accessPolicies/<access-policy-id>/accessLevels/<my-access-level-name>\" in request.auth.access_levels"
     }
 }
