b688c76618eff8ef1d023034d36703b3194497aa
paulmsullivan <paulmsullivan@outlook.com>
2025-08-01 13:38:58 -0400

    Update iap.tf
    Signed-off-by: paulmsullivan <paulmsullivan@outlook.com>

--- gcp/iap.tf
+++ gcp/iap.tf
@@ -12,14 +12,15 @@ resource "google_access_context_manager_access_level" "access-level2" {
   basic {
     conditions {
       regions = ["US"]
-     }
-  }
-  vpc_network_sources = {
-    "vpc_labvms" = {
-      network_id = "projects/cogent-dragon-379819/global/networks/paullab-vpc"
     }
+    vpc_network_sources = {
+      "vpc_labvms" = {
+        network_id = "projects/cogent-dragon-379819/global/networks/paullab-vpc"
+      }
+    }
+    combining_function = "OR"
   }
-  combining_function = "OR"
+
 }
 
 
