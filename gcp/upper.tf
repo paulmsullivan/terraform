95417757c4101ec66d2d70f8f7bab6d393a6ad56
paulmsullivan <paulmsullivan@outlook.com>
2025-09-16 13:33:24 -0400

    added upper project

--- gcp/upper.tf
+++ gcp/upper.tf
@@ -4,3 +4,10 @@ resource "google_folder" "upper" {
   parent       = "organizations/${var.org_id}"
 }
 
+resource "google_project" "upperproject" {
+    name            = "upperproject"
+    project_id      = "upperproject"
+    billing_account = var.billing_account
+    folder_id       = google_folder.upper.name
+    auto_create_network = false
+}
