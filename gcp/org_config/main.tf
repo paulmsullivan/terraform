a4d8177b2d54bbc6ccd5fe8c3cc2a96796f3bb86
paulmsullivan <paulmsullivan@outlook.com>
2025-09-16 14:09:11 -0400

    Update main.tf
    Signed-off-by: paulmsullivan <paulmsullivan@outlook.com>

--- gcp/org_config/main.tf
+++ gcp/org_config/main.tf
@@ -16,6 +16,11 @@ variable "gcp-creds" {
   default = ""
 }
 
+variable "gcp-billing-account" {
+  type    = string
+  default = ""
+}
+
 #
 # Credentials for Terraform to auth to GCP for operations.
 # The values set here will be inherited by the resources
